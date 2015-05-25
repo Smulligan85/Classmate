3b7ebb749d4648e3b223e3e2bc2278e102d67ef8
Sean Mulligan <sean.mulligan85@gmail.com>
2015-05-25 10:19:51 -0700

    Adding delete button to note edit page

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,37 +1,43 @@
 class NotesController < ApplicationController
 
-  before_action :find_note, only: [:update, :edit, :destroy]
+  before_action :find_note, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @notes = Note.all
+  end
 
   def new
     @project = Project.find(params[:project_id])
-    @note = @project.notes.build
+    @note = current_user.notes.build
   end
 
   def create
     @project = Project.find(params[:project_id])
     @note = @project.notes.build(note_params)
-    @note.project = @project
-    @note.save ? flash[:notice] = "Note saved." : flash[:error] = "Note could not be saved, please try again."
+    @note.save ? flash[:notice] = "Note created." : flash[:error] = "Note could not be created, please try again."
     redirect_to [current_user, @project]
+  end
+
+  def show
+  end
+
+  def edit
   end
 
   def update
     if @note.update_attributes(note_params)
       @note.save
       flash[:notice] = "Note updated."
-      redirect_to @project
+      redirect_to authenticated_root_path
     else
-      flash[:error] = "Could not save note."
+      flash[:error] = "Could not update note."
       render :edit
     end
   end
 
-  def edit
-  end
-
   def destroy
     @note.delete ? flash[:notice] = "Note deleted." : flash[:error] = "Note could not be deleted."
-    redirect_to user_project_path
+    redirect_to [current_user, @project]
   end
 
   private
